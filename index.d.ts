b14a11a9f99f6a947b26123e4dade76f6df9e2b2
Jesse Ditson <jesse.ditson@gmail.com>
2025-06-10 17:57:14 -0700

    fix global API

--- index.d.ts
+++ index.d.ts
@@ -1,2 +1,11 @@
 import "./editor-types";
+import { ArchivalEditor } from "archival-custom-editor-types";
+
 export * from "archival-custom-editor-types";
+
+declare global {
+  interface Window {
+    ArchivalEditor: ArchivalEditor<any>;
+    INJECTED_VALUES: Object;
+  }
+}
